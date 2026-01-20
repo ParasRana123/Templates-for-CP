9fe8d596faa0253624e7738be2d52802066bccda
ParasRana123 <parasrana579@gmail.com>
2026-01-20 17:16:11 +0530

    segment tree designing

--- segment_tree/designing.cpp
+++ segment_tree/designing.cpp
@@ -36,6 +36,59 @@ int query(int ind , int low , int high , int l , int r) {
     return max(left , right);
 }
 
+void pointUpdate(int ind , int low , int high , int node , int val) {
+    if(low == high) {
+        seg[ind] += val;
+    } else {
+        int mid = (low + high) / 2;
+        if(node <= mid && node >= low) pointUpdate(2 * ind + 1 , low , mid , node , val);
+        else pointUpdate(2 * ind + 2 , mid + 1 , high , node , val);
+        seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+    }
+}
+
+void rangeUpdate(int ind , int low , int high , int l , int r , int val) {
+    if(lazy[ind] != -1) {
+        seg[ind] += (high - low + 1) * lazy[ind];
+        if(low != high) {
+            lazy[2 * ind + 1] += lazy[ind];
+            lazy[2 * ind + 2] += lazy[ind];
+        }
+        lazy[ind] = 0;
+    }
+    if(r < low || l > high || low > high) return;
+    if(low >= l && high <= r) {
+        seg[ind] += (high - low + 1) * val;
+        if(low != high) {
+            lazy[2 * ind + 1] += lazy[ind];
+            lazy[2 * ind + 2] += lazy[ind];
+        }
+        return;
+    }
+    ind mid = (low + high) >> 1;
+    rangeUpdate(2 * ind + 1 , low , mid , l , r , val);
+    rangeUpdate(2 * ind + 2 , mid + 1 , high , l , r , val);
+    seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+}
+
+int querySumLazy(int ind , int low , int high , int l , int r , int val) {
+    if(lazy[ind] != 0) {
+        seg[ind] += (high - low + 1) * lazy[ind];
+        if(low != high) {
+            lazy[2 * ind + 1] += lazy[ind];
+            lazy[2 * ind + 2] += lazy[ind];
+        }
+        lazy[ind] = 0;
+    }
+    if(r < low || l > high || low > high) return 0;
+    if(low >= l && high <= r) {
+        return seg[ind];
+    }
+    int mid = (low + high) >> 1;
+    return querySumLazy(2 * ind + 1 , low , mid , l , r , val)
+          +querySumLazy(2 * ind + 2 , mid + 1 , high , l , r , val);
+}
+
 int main() {
     int n;
     cin >> n;
