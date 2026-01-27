834273b8fe5027cc31a0b7b1a0e92fe04c76e45d
ParasRana123 <parasrana579@gmail.com>
2026-01-27 19:47:50 +0530

    added graphs dijkstra 2 methods

---
+++ graphs/Dijkstra/pq.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+// Will be storing the nodes as well as the respective distances/cost between them in the adjacency list
+// will use a min heap of {node , dist} --> Push the initial node 0 with a dist of 0 into the heap
+// also we will maintain a dist vector alongside --> will initialise the src node with a dist of 0 and all the other nodes with a initail dist of infinite(INT_MAX)
+
+vector<int> dijkstra(int V , vector<vector<int>> adj , int S) {
+    priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>> pq;
+    vector<int> dist(V);
+    for(int i = 0 ; i < V ; i++) {
+        dist[i] = 1e9;
+    }
+    dist[S] = 0;
+    pq.push({0 , S});
+    while(!pq.empty()) {
+        int dis = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
+        for(auto it: adj[node]) {
+            int edgeWeight = it[1];
+            int adjNode = it[0];
+            if(dis + edgeWeight < dist[adjNode]) {
+                dist[adjNode] = dis + edgeWeight;
+                pq.push({dist[adjNode] , adjNode});
+            }
+        }
+    }
+    return dist;
+}
+
+int main() {
+
+}
---
+++ graphs/Dijkstra/set.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+vector<int> dijkstra(int V , vector<vector<pair<int , int>>> &adj , int src) {
+    set<pair<int , int>> st;
+    vector<int> dist(V , 1e18);
+    st.insert({0 , src});
+    dist[src] = 0;
+    while(!st.empty()) {
+        auto it = *(st.begin());
+        int dis = it.first;
+        int node = it.second;
+        st.erase(it);
+
+        for(auto &it: adj[node]) {
+            int edgeW = it.first;
+            int adjNode = it.second;
+            if(edgeW + dis < dist[adjNode]) {
+                if(dist[adjNode] != 1e18) {
+                    st.erase({dist[adjNode] , adjNode});
+                }
+                dist[adjNode] = edgeW + dis;
+                st.insert({dist[adjNode] , adjNode});
+             }
+        }
+    }
+    return dist;
+}
